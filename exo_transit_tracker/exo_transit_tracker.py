86dce643bf9f6e4f32424a80b1ee4e1aa3faa3ce
Paul Dalba <pauldalba89@gmail.com>
2023-05-06 15:38:29 -0700

    minor: Indent format adjustments

--- exo_transit_tracker/exo_transit_tracker.py
+++ exo_transit_tracker/exo_transit_tracker.py
@@ -9,202 +9,200 @@ from exo_transit_tracker.utils import query_nea
 from exo_transit_tracker.visibility import calc_vis
 
 def next_transit(st_name, pl_id, source='NEA', P=None, T0=None, loc=None):
-	"""
-		Determine the timing of the next transit of a given exoplanet.
-	
-		Inputs
-		------
-		
-			st_name: str
-				Name of the host star. Must be resolvable by the CDS
-				Simbad service.
-	
-			pl_id: str
-				Letter or identifier of the exoplanet.
-			
-			source: str
-				Where to draw the exoplanet transit ephemeris from. 
-					Options include:
-						'NEA': exoplanet data are downloaded from the 
-							   NASA Exoplanet Archive and queried.
-						None: the transit ephemeris (P and T0) must 
-							  be provided.
-				
-			P: float or NoneType
-				The orbital period (in days) for the planet. Must be
-				provided if source is None.
-			
-			T0: float or NoneType
-				The transit (or conjunction) time for the planet in 
-				Barycentric Julian Days. Must be provided if source 
-				is None.
-		
-			loc: tuple / list or None
-				A tuple containing the longitude (in degrees), 
-				latitude (in degrees), and elevation (in meters) for 
-				the location on Earth to be treated as the observer.
-				If provided, the next_transit will be that which is 
-				visible to this observer.
-					
-		Outputs
-		-------
-	
-			nextTransit: astropy.time.Time
-				A Time object of the next transit mid-time for this planet, for
-				the given (or determined) ephemeris, visible from the given 
-				location (if applicable).			
-	"""
+    """
+        Determine the timing of the next transit of a given exoplanet.
+    
+        Inputs
+        ------
+        
+            st_name: str
+                Name of the host star. Must be resolvable by the CDS
+                Simbad service.
+    
+            pl_id: str
+                Letter or identifier of the exoplanet.
+            
+            source: str
+                Where to draw the exoplanet transit ephemeris from. 
+                    Options include:
+                        'NEA': exoplanet data are downloaded from the 
+                               NASA Exoplanet Archive and queried.
+                        None: the transit ephemeris (P and T0) must 
+                              be provided.
+                
+            P: float or NoneType
+                The orbital period (in days) for the planet. Must be
+                provided if source is None.
+            
+            T0: float or NoneType
+                The transit (or conjunction) time for the planet in 
+                Barycentric Julian Days. Must be provided if source 
+                is None.
+        
+            loc: tuple / list or None
+                A tuple containing the longitude (in degrees), 
+                latitude (in degrees), and elevation (in meters) for 
+                the location on Earth to be treated as the observer.
+                If provided, the next_transit will be that which is 
+                visible to this observer.
+                    
+        Outputs
+        -------
+    
+            nextTransit: astropy.time.Time
+                A Time object of the next transit mid-time for this planet, for
+                the given (or determined) ephemeris, visible from the given 
+                location (if applicable).
+    """
 
-	# Parse args
-	if source not in ['NEA', None]:
-		raise ValueError('Source of ephemeris must be "NEA" or None.')
-	elif source is None:
-		if P is None or T0 is None:
-			raise ValueError('Ephmeris must be provided if source is None.')
-	
-	if loc is not None:
-		if len(loc) != 3:
-			msg = 'loc must a be 3 item tuple or list containing'
-			msg += 'longitude, latitude, and elevation.'
-			raise ValueError(msg)
-		try:
-			loc = EarthLocation.from_geodetic(loc[0], loc[1], height=loc[2])
-		except Exception as ee:
-			print(ee)
-			
-	# If source is NEA and ephemeris is given, default to ephemeris
-	if P is not None and T0 is not None and source == 'NEA':
-		msg = 'Using given ephemeris as opposed to '
-		msg += 'querying NEA.'
-		warnings.warn(msg)
-		source = None
-	
-	# Create an Exoplanet object and calculate the next transit from now
-	xo = Exoplanet(st_name, pl_id, P=P, T0=T0)
-	 	
-	# CASE: determine transit timing with given ephemeris
-	if source is None and loc is None:
-		xo.ephemeris_to_next_transit()
-		return xo.next_transit
-		
-	# CASE: determine transit timing with given ephemeris with a location
-	elif source is None and loc is not None:
-		# Query simbad for hostName
-		xo.get_radec_simbad()
-		xo.simbad_known = True
-		
-		# Calculate next transit time
-		xo.ephemeris_to_next_transit()
-		
-		# Calculate visibility
-		vis = calc_vis(xo, loc)
-		
-		if not vis:
-			print('Sorry, this star is never visible from this location.' \
-				  ' No transits visible.')
-			return
-		
-		return xo.next_transit
-		
-	# CASE: get ephemeris from NEA
-	else:
-		# Query simbad for hostName
-		xo.get_radec_simbad()
-		xo.simbad_known = True
-		
-		# Query the NEA for all exoplanet RA/Dec
-		qryCols = 'pl_name,ra,dec'
-		qryTab = 'ps'
-		qryConst = {'col': ['default_flag'],
-					'oper': ['='],
-					'val': ['1']}
-		df_coords = query_nea(col=qryCols, tab=qryTab, const=qryConst)
-							
-		# Cross match with the target system's RA/Dec
-		df_coords['dist'] = np.sqrt((df_coords.ra - xo.ra_deg) ** 2 +
-								    (df_coords.dec - xo.dec_deg) ** 2)
-		matchInd = df_coords.where(df_coords.dist ==
-								   min(df_coords.dist))['dist'].dropna().index
-		matchName = df_coords.loc[matchInd, 'pl_name'].values[0]
-		if xo.st_name not in matchName:
-			msg = 'Matched {} on'.format(xo.st_name)
-			msg += ' the NEA to {}. '.format(matchName)
-			msg += 'Assuming alias and proceeding.'
-			warnings.warn(msg)
-		
-		# Gather all the ephemerides for this planet from the
-		# NEA with another query. 
-		qryCols = 'pl_name,default_flag,tran_flag,pl_orbper,pl_tranmid'
-		qyrTab = 'ps'
-		df_pl = query_nea(col=qryCols, tab=qryTab)
-		
-		# Trim other planets
-		df_pl = df_pl.loc[df_pl.where(df_pl['pl_name'] == 
-								      matchName)['pl_name'].dropna().index]
-		
-		
-		# Evaluate the results and pick an ephemeris
-		df_sub = df_pl.loc[df_pl.where(df_pl['tran_flag'] == 
-								       1)['tran_flag'].dropna().index]
-		if len(df_sub) == 0:
-			print('According to the NEA, {} does not transit. '.format(xo.pl_name))
-			print('If you have discovered a transit of this planet, '
-				  'please consult your nearest observatory immediately.')
-			return 
-		xo.shows_transits = True
-		
-		# Check the default entry
-		df_default = df_pl.loc[df_pl.where(df_pl['default_flag'] == 
-								       1)['default_flag'].dropna().index]
-		if df_default['pl_orbper'].values[0] is not None and \
-			df_default['pl_tranmid'].values[0] is not None:
-				xo.P = df_default['pl_orbper'].values[0]
-				xo.T0 = df_default['pl_tranmid'].values[0]
-		else:
-			# Use an entry that gives both P and T0 or use multiple entries
-			df_other = df_pl.loc[df_pl.where(df_pl['default_flag'] == 
-								       0)['default_flag'].dropna().index]
-			df_other['full_ephem'] = [False] * len(df_other)
-			final_P, final_T0 = None, None
-			for i in df_other.index:
-				if df_other.loc[i, 'pl_orbper'] is not None and \
-					df_other.loc[i, 'pl_tranmid'] is not None:
-						xo.P = df_other.loc[i, 'pl_orbper']
-						xo.T0 = df_other.loc[i, 'pl_tranmid']
-						break
-				else:
-					if df_other.loc[i, 'pl_orbper'] is not None and final_P is None:
-						final_P = df_other.loc[i, 'pl_orbper']
-					if df_other.loc[i, 'pl_tranmid'] is not None and final_T0 is None:
-						final_T0 = df_other.loc[i, 'pl_tranmid']
-			
-			if xo.P is None and final_P is not None:
-				xo.P = final_P
-			if xo.T0 is None and final_T0 is not None:
-				xo.T0 = final_T0
-			if np.any([xo.P is None, xo.T0 is None]):
-				print('Could not extract a complete ephemeris from the NEA.'
-					  ' No transits can be reported.')
-				return
-								
-		# Calculate the next transit from now
-		xo.ephemeris_to_next_transit()
-									
-		# If no location, simply return. Otherwise, check visibility
-		if loc is None:
-			return xo.next_transit
-		else:
-			# Calculate visibility
-			vis = calc_vis(xo, loc)
-		
-			if not vis:
-				print('Sorry, this star is never visible from this location.' \
-				  	  ' No transits visible.')
-				return
-		
-			return xo.next_transit
-			
-			
+    # Parse args
+    if source not in ['NEA', None]:
+        raise ValueError('Source of ephemeris must be "NEA" or None.')
+    elif source is None:
+        if P is None or T0 is None:
+            raise ValueError('Ephmeris must be provided if source is None.')
+    
+    if loc is not None:
+        if len(loc) != 3:
+            msg = 'loc must a be 3 item tuple or list containing'
+            msg += 'longitude, latitude, and elevation.'
+            raise ValueError(msg)
+        try:
+            loc = EarthLocation.from_geodetic(loc[0], loc[1], height=loc[2])
+        except Exception as ee:
+            print(ee)
+            
+    # If source is NEA and ephemeris is given, default to ephemeris
+    if P is not None and T0 is not None and source == 'NEA':
+        msg = 'Using given ephemeris as opposed to '
+        msg += 'querying NEA.'
+        warnings.warn(msg)
+        source = None
+    
+    # Create an Exoplanet object and calculate the next transit from now
+    xo = Exoplanet(st_name, pl_id, P=P, T0=T0)
+         
+    # CASE: determine transit timing with given ephemeris
+    if source is None and loc is None:
+        xo.ephemeris_to_next_transit()
+        return xo.next_transit
+        
+    # CASE: determine transit timing with given ephemeris with a location
+    elif source is None and loc is not None:
+        # Query simbad for hostName
+        xo.get_radec_simbad()
+        xo.simbad_known = True
+        
+        # Calculate next transit time
+        xo.ephemeris_to_next_transit()
+        
+        # Calculate visibility
+        vis = calc_vis(xo, loc)
+        
+        if not vis:
+            print('Sorry, this star is never visible from this location.' \
+                  ' No transits visible.')
+            return
+        
+        return xo.next_transit
+        
+    # CASE: get ephemeris from NEA
+    else:
+        # Query simbad for hostName
+        xo.get_radec_simbad()
+        xo.simbad_known = True
+        
+        # Query the NEA for all exoplanet RA/Dec
+        qryCols = 'pl_name,ra,dec'
+        qryTab = 'ps'
+        qryConst = {'col': ['default_flag'],
+                    'oper': ['='],
+                    'val': ['1']}
+        df_coords = query_nea(col=qryCols, tab=qryTab, const=qryConst)
+                            
+        # Cross match with the target system's RA/Dec
+        df_coords['dist'] = np.sqrt((df_coords.ra - xo.ra_deg) ** 2 +
+                                    (df_coords.dec - xo.dec_deg) ** 2)
+        matchInd = df_coords.where(df_coords.dist ==
+                                   min(df_coords.dist))['dist'].dropna().index
+        matchName = df_coords.loc[matchInd, 'pl_name'].values[0]
+        if xo.st_name not in matchName:
+            msg = 'Matched {} on'.format(xo.st_name)
+            msg += ' the NEA to {}. '.format(matchName)
+            msg += 'Assuming alias and proceeding.'
+            warnings.warn(msg)
+        
+        # Gather all the ephemerides for this planet from the
+        # NEA with another query. 
+        qryCols = 'pl_name,default_flag,tran_flag,pl_orbper,pl_tranmid'
+        qyrTab = 'ps'
+        df_pl = query_nea(col=qryCols, tab=qryTab)
+        
+        # Trim other planets
+        df_pl = df_pl.loc[df_pl.where(df_pl['pl_name'] == 
+                                      matchName)['pl_name'].dropna().index]
+        
+        
+        # Evaluate the results and pick an ephemeris
+        df_sub = df_pl.loc[df_pl.where(df_pl['tran_flag'] == 
+                                       1)['tran_flag'].dropna().index]
+        if len(df_sub) == 0:
+            print('According to the NEA, {} does not transit. '.format(xo.pl_name))
+            print('If you have discovered a transit of this planet, '
+                  'please consult your nearest observatory immediately.')
+            return 
+        xo.shows_transits = True
+        
+        # Check the default entry
+        df_default = df_pl.loc[df_pl.where(df_pl['default_flag'] == 
+                                       1)['default_flag'].dropna().index]
+        if df_default['pl_orbper'].values[0] is not None and \
+            df_default['pl_tranmid'].values[0] is not None:
+                xo.P = df_default['pl_orbper'].values[0]
+                xo.T0 = df_default['pl_tranmid'].values[0]
+        else:
+            # Use an entry that gives both P and T0 or use multiple entries
+            df_other = df_pl.loc[df_pl.where(df_pl['default_flag'] == 
+                                       0)['default_flag'].dropna().index]
+            df_other['full_ephem'] = [False] * len(df_other)
+            final_P, final_T0 = None, None
+            for i in df_other.index:
+                if df_other.loc[i, 'pl_orbper'] is not None and \
+                    df_other.loc[i, 'pl_tranmid'] is not None:
+                        xo.P = df_other.loc[i, 'pl_orbper']
+                        xo.T0 = df_other.loc[i, 'pl_tranmid']
+                        break
+                else:
+                    if df_other.loc[i, 'pl_orbper'] is not None and final_P is None:
+                        final_P = df_other.loc[i, 'pl_orbper']
+                    if df_other.loc[i, 'pl_tranmid'] is not None and final_T0 is None:
+                        final_T0 = df_other.loc[i, 'pl_tranmid']
+            
+            if xo.P is None and final_P is not None:
+                xo.P = final_P
+            if xo.T0 is None and final_T0 is not None:
+                xo.T0 = final_T0
+            if np.any([xo.P is None, xo.T0 is None]):
+                print('Could not extract a complete ephemeris from the NEA.'
+                      ' No transits can be reported.')
+                return
+                                
+        # Calculate the next transit from now
+        xo.ephemeris_to_next_transit()
+                                    
+        # If no location, simply return. Otherwise, check visibility
+        if loc is None:
+            return xo.next_transit
+        else:
+            # Calculate visibility
+            vis = calc_vis(xo, loc)
+        
+            if not vis:
+                print('Sorry, this star is never visible from this location.' \
+                        ' No transits visible.')
+                return
+        
+            return xo.next_transit
 
 
 
--- exo_transit_tracker/visibility.py
+++ exo_transit_tracker/visibility.py
@@ -5,104 +5,104 @@ import astroplan as ap
 import warnings
 
 def calc_vis(xo, loc, night='nautical', altMin=15, moonSep=30):
-	"""
-		Calculate the visibility of the host star from the given site and a
-		list of constraints and determine when the next visible transit occurs.
-		
-		Inputs
-		------
-		
-			xo: Exoplanet object
-				Exoplanet object containing a time in the next_transit attribute
-				
-			loc: EarthLocation 
-				astropy.coordinates.EarthLocation object for the observer.
-				
-			night: str (Optional)
-				Identifier of the kind of night to be used for checking visibility.
-				Must be one of:
-					sunset: Sun 0 deg below horizon
-					civil: Sun 6 deg below horizon
-					nautical: Sun 12 deg below horizon
-					astronomical: Sun 18 deg below horizon
-					
-			altMin: float (Optional)
-				Minimum altitude (in degrees) to be used for checking visibility.
-				Must be in the range [0, 90].
-				
-			moonSep: float (Optional)
-				Minimum angular separation from the Moon to be used for checking
-				visibility. Must be greater than 0.
-		
-		Outputs
-		-------
-			True, if the current transit in xo.next_transits is visible
-			False, if the star is never visible from this source
-	"""
-	
-	# Check visibility args
-	if night.lower() not in ['sunset', 'civil', 'nautical', 'astronomical']:
-		raise ValueError('{} is not a recognized definition of night time.'.format(night))
-	if altMin < 0:
-		warnings.warn('Minimum altitude cannot be less than 0 deg. Setting to 0 deg.')
-		altMin = 0
-	elif altMin >= 90:
-		raise ValueError('Minimum altitude cannot be >= 90 deg.')
-	if moonSep < 0:
-		msg = 'Minimum Moon separation cannot be less than 0 deg. '
-		msg += 'Setting to 0 deg.'
-		warnings.warn(msg)
-		moonSep = 0
-	
-	# Make sure Exoplanet object has a next_transit attribute computed
-	if xo.next_transit is None:
-		msg = 'Visibility is being evaluated before a transit '
-		msg += 'time has been computed. Computing now...'
-		warnings.warn(msg)
-		xo.ephemeris_to_next_transit()
-		
-	# Build astroplan constraints list
-	Constraints = []
-	if night == 'sunset':
-		Constraints.append(ap.AtNightConstraint(max_solar_altitude = 0 * u.deg))
-	elif night == 'civil':
-		Constraints.append(ap.AtNightConstraint.twilight_civil())
-	elif night == 'nautical':
-		Constraints.append(ap.AtNightConstraint.twilight_nautical())
-	else:
-		Constraints.append(ap.AtNightConstraint.twilight_astronomical())
-	
-	Constraints.append(ap.AltitudeConstraint(min=altMin * u.deg))
-	Constraints.append(ap.MoonSeparationConstraint(min=moonSep * u.deg))
-	
-	Observer = ap.Observer(location=loc, name='Observer')
-	
-	# First check if this source is ever observable from this location
-	all_year = Time(['2023-01-01T00:00:00', '2023-12-31T23:59:59'])
-	ever_observable = ap.is_observable(Constraints, Observer, xo.st_coords,
-									   time_range=all_year)[0]
-	if not ever_observable:
-		return False
-	
-	# Continue to determine the next observable transit. Start by checking the 
-	# transit time already determined for the Exoplanet object
-	vis = ap.is_observable(Constraints, Observer, xo.st_coords,
-						   times=xo.next_transit)[0]
+    """
+        Calculate the visibility of the host star from the given site and a
+        list of constraints and determine when the next visible transit occurs.
+        
+        Inputs
+        ------
+        
+            xo: Exoplanet object
+                Exoplanet object containing a time in the next_transit attribute
+                
+            loc: EarthLocation 
+                astropy.coordinates.EarthLocation object for the observer.
+                
+            night: str (Optional)
+                Identifier of the kind of night to be used for checking visibility.
+                Must be one of:
+                    sunset: Sun 0 deg below horizon
+                    civil: Sun 6 deg below horizon
+                    nautical: Sun 12 deg below horizon
+                    astronomical: Sun 18 deg below horizon
+                    
+            altMin: float (Optional)
+                Minimum altitude (in degrees) to be used for checking visibility.
+                Must be in the range [0, 90].
+                
+            moonSep: float (Optional)
+                Minimum angular separation from the Moon to be used for checking
+                visibility. Must be greater than 0.
+        
+        Outputs
+        -------
+            True, if the current transit in xo.next_transits is visible
+            False, if the star is never visible from this source
+    """
+    
+    # Check visibility args
+    if night.lower() not in ['sunset', 'civil', 'nautical', 'astronomical']:
+        raise ValueError('{} is not a recognized definition of night time.'.format(night))
+    if altMin < 0:
+        warnings.warn('Minimum altitude cannot be less than 0 deg. Setting to 0 deg.')
+        altMin = 0
+    elif altMin >= 90:
+        raise ValueError('Minimum altitude cannot be >= 90 deg.')
+    if moonSep < 0:
+        msg = 'Minimum Moon separation cannot be less than 0 deg. '
+        msg += 'Setting to 0 deg.'
+        warnings.warn(msg)
+        moonSep = 0
+    
+    # Make sure Exoplanet object has a next_transit attribute computed
+    if xo.next_transit is None:
+        msg = 'Visibility is being evaluated before a transit '
+        msg += 'time has been computed. Computing now...'
+        warnings.warn(msg)
+        xo.ephemeris_to_next_transit()
+        
+    # Build astroplan constraints list
+    Constraints = []
+    if night == 'sunset':
+        Constraints.append(ap.AtNightConstraint(max_solar_altitude = 0 * u.deg))
+    elif night == 'civil':
+        Constraints.append(ap.AtNightConstraint.twilight_civil())
+    elif night == 'nautical':
+        Constraints.append(ap.AtNightConstraint.twilight_nautical())
+    else:
+        Constraints.append(ap.AtNightConstraint.twilight_astronomical())
+    
+    Constraints.append(ap.AltitudeConstraint(min=altMin * u.deg))
+    Constraints.append(ap.MoonSeparationConstraint(min=moonSep * u.deg))
+    
+    Observer = ap.Observer(location=loc, name='Observer')
+    
+    # First check if this source is ever observable from this location
+    all_year = Time(['2023-01-01T00:00:00', '2023-12-31T23:59:59'])
+    ever_observable = ap.is_observable(Constraints, Observer, xo.st_coords,
+                                       time_range=all_year)[0]
+    if not ever_observable:
+        return False
+    
+    # Continue to determine the next observable transit. Start by checking the 
+    # transit time already determined for the Exoplanet object
+    vis = ap.is_observable(Constraints, Observer, xo.st_coords,
+                           times=xo.next_transit)[0]
 
-	if vis:
-		return True
-		
-	while not vis:
-		xo.ephemeris_to_next_transit(t=xo.next_transit + 1 * u.hour)
-		with warnings.catch_warnings():
-			warnings.simplefilter("ignore")
-			vis = ap.is_observable(Constraints, Observer, xo.st_coords,
-								   times=xo.next_transit)[0]
-		
-		# Rip-cord for this while loop
-		if xo.next_transit.byear > 10000:
-			msg = 'No visibility found up to the year 10,000 (!!).'
-			msg += ' Stopping analysis here.'
-			raise RuntimeError(msg)
-	
-	return True
+    if vis:
+        return True
+        
+    while not vis:
+        xo.ephemeris_to_next_transit(t=xo.next_transit + 1 * u.hour)
+        with warnings.catch_warnings():
+            warnings.simplefilter("ignore")
+            vis = ap.is_observable(Constraints, Observer, xo.st_coords,
+                                   times=xo.next_transit)[0]
+        
+        # Rip-cord for this while loop
+        if xo.next_transit.byear > 10000:
+            msg = 'No visibility found up to the year 10,000 (!!).'
+            msg += ' Stopping analysis here.'
+            raise RuntimeError(msg)
+    
+    return True
